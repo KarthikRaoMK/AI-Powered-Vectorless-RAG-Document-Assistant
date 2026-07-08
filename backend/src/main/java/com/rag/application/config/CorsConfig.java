8aa2ce4ef0233582fe03f664d1534446723c3826
Karthik <raokarthik946@gmail.com>
2026-07-08 11:39:00 +0530

    Add CORS configuration

--- backend/src/main/java/com/rag/application/config/CorsConfig.java
+++ backend/src/main/java/com/rag/application/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://coruscating-douhua-a66542.netlify.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
 
